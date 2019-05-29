be763ddc8db65fe82cbe7f62bb252366c774783b
Dustin Schoenbrun <dustin.schoenbrun@nutanix.com>
2019-05-29 09:46:09 -0400

    Modify reverse.py, add hypothesis unit tests

--- reverse.py
+++ reverse.py
@@ -1,10 +1,7 @@
-def reverse(a_string: str):
+def reverse_string(a_string: str):
   """Take the input a_string and return it reversed (e.g. "hello" becomes
   "olleh"."""
   reversed_string = ""
   for i in range(len(a_string)):
     reversed_string += a_string[-i]
   return reversed_string
-
-
-
---
+++ test_reverse_hypothesis.py
@@ -0,0 +1,15 @@
+import unittest
+
+from hypothesis import example, given
+from hypothesis.strategies import text
+
+from reverse import reverse_string
+
+class TestReverse(unittest.TestCase):
+  @given(text())
+  def test_reversing_reversed_string_gives_original(self, a_str):
+    assert a_str == reverse_string(reverse_string(a_str))
+
+  @given(text())
+  def test_reverse_string_equal_to_reverse(self, a_str):
+    assert a_str[::-1] == reverse_string(a_str)
