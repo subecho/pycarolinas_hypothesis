52347c71152239f819b2769fdb664125b5ed0245
Dustin Schoenbrun <dustin.schoenbrun@nutanix.com>
2019-05-28 22:38:23 -0400

    Add reverse.py as a simple string reverser

---
+++ reverse.py
@@ -0,0 +1,10 @@
+def reverse(a_string: str):
+  """Take the input a_string and return it reversed (e.g. "hello" becomes
+  "olleh"."""
+  reversed_string = ""
+  for i in range(len(a_string)):
+    reversed_string += a_string[-i]
+  return reversed_string
+
+
+
